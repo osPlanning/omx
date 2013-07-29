fa3013d0fb0a0e4bdcb8d0e6f0a02a2d0bdab979
Billy Charlton <bcharlton@psrc.org>
2013-07-29 12:54:25 -0700

    Make createMatrix() pytables 2.4-compatible
    - 'obj' parameter on createCArray method required pytables >=3.0

--- omx/File.py
+++ omx/File.py
@@ -1,18 +1,30 @@
 # OMX package
 # release 1
 
-import tables;
+import tables;  # requires pytables >= 2.4
 
 class File(tables.File):
+
     def createMatrix(self, name, atom=None, shape=None, title='', filters=None,
                      chunkshape=None, byteorder=None, createparents=False, obj=None,
                      tags=None):
         """Create OMX Matrix (CArray) at root level. User must pass in either
            an existing numpy matrix, or a shape and an atom type."""
-        matrix = self.createCArray(self.root, name, atom, shape, title, filters, 
-                                   chunkshape, byteorder, createparents, obj)
+
+        if tables.__version__.startswith('3'):
+            matrix = self.createCArray(self.root, name, atom, shape, title, filters,
+                                       chunkshape, byteorder, createparents, obj)
+        else:
+            # this version is tables 2.4-compatible:
+            matrix = self.createCArray(self.root, name, atom, shape, title, filters,
+                                       chunkshape, byteorder, createparents)
+            if (obj != None):
+                matrix[:] = obj
+
         matrix.attrs.tags = tags
+
         return matrix
+
 
     def shape(self):
         """Return the one and only shape of all matrices in this File"""
@@ -21,9 +33,11 @@ class File(tables.File):
         else:
             return None
 
+
     def listMatrices(self):
         """Return list of Matrix names in this File"""
         return self.root._v_children
+
 
     def listAllTags(self):
         """Return combined list of all tags used for any Matrix in this File"""
@@ -31,7 +45,7 @@ class File(tables.File):
         for m in self.listNodes(self.root):
             if 'tags' in m.attrs and m.attrs.tags != None:
                 all_tags.update(m.attrs.tags)
-        return list(all_tags)
+        return sorted(list(all_tags))
 
 
 
@@ -56,8 +70,13 @@ class File(tables.File):
     def __len__(self):
         return len(self.root._v_children)
 
-    def __setitem__(self, key, value):
-        return self.createMatrix(key, obj=value)
+    def __setitem__(self, key, dataset):
+        # We need to determine atom and shape from the object that's been passed in.
+        # This assumes 'dataset' is a numpy object.
+        atom = tables.Atom.from_dtype(dataset.dtype)
+        shape = dataset.shape
+
+        return self.createMatrix(key, atom, shape, obj=dataset)
 
     def __delitem__(self, key):
         self.removeNode(self.root, key)
