565f90544d7707d833b1c818802c04ad4c2b777d
Billy Charlton <billy@worldofbilly.com>
2015-01-15 15:16:37 -0500

    Add pip-installer files and move things around to support pip.

---
+++ api/python/README.txt
@@ -0,0 +1,10 @@
+===========
+OpenMatrix:  OMX
+===========
+
+OMX is an open matrix data format, designed for sharing big matrix
+data.
+
+Read more about it at the project home page:
+https://sites.google.com/site/openmodeldata
+
---
+++ api/python/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='OpenMatrix',
+    version='0.2.0',
+    author='Billy Charlton',
+    author_email='billy@worldofbilly.com',
+    packages=['openmatrix', 'openmatrix.test'],
+    url='http://pypi.python.org/pypi/OpenMatrix/',
+    license='LICENSE.txt',
+    description='OMX, the open matrix data format.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "tables >= 2.3.0",
+        "numpy >= 1.8.2",
+    ],
+)
