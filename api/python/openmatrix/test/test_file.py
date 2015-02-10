7349a3c01062ecfb6da1a36af7214886627cfeb9
Matt Davis <jiffyclub@gmail.com>
2015-02-10 15:02:33 -0800

    Use with context manager for opening OMX files
    This ensures that all the files are properly closed
    when the tests are done with them.

--- api/python/openmatrix/test/test_file.py
+++ api/python/openmatrix/test/test_file.py
@@ -28,65 +28,60 @@ def teardown_func():
 
 @with_setup(setup_func, teardown_func)
 def test_create_file():
-    f = omx.openFile(TEST_FILE, 'w')
-    f.close()
+    with omx.openFile(TEST_FILE, 'w'):
+        pass
     assert os.path.isfile(TEST_FILE)
 
 
 @with_setup(setup_func, teardown_func)
 def test_open_readonly_hdf5_file():
-    f = tables.openFile(TEST_FILE, 'w')
-    f.close()
-    f = omx.openFile(TEST_FILE, 'r')
-    f.close()
+    with tables.openFile(TEST_FILE, 'w'):
+        pass
+    with omx.openFile(TEST_FILE, 'r'):
+        pass
 
 
 @with_setup(setup_func, teardown_func)
 def test_add_numpy_matrix_using_brackets():
-    f = omx.openFile(TEST_FILE, 'w')
-    f['m1'] = np.ones((5,5))
-    f.close()
+    with omx.openFile(TEST_FILE, 'w') as f:
+        f['m1'] = np.ones((5,5))
 
 
 @with_setup(setup_func, teardown_func)
 def test_add_numpy_matrix_using_create_matrix():
-    f = omx.openFile(TEST_FILE, 'w')
-    f.createMatrix('m1', obj=np.ones((5,5)))
-    f.close()
+    with omx.openFile(TEST_FILE, 'w') as f:
+        f.createMatrix('m1', obj=np.ones((5,5)))
 
 
 @with_setup(setup_func, teardown_func)
 @raises(tables.FileModeError)
 def test_add_matrix_to_readonly_file():
-    f = omx.openFile(TEST_FILE, 'w')
-    f['m2'] = np.ones((5,5))
-    f.close()
-    f = omx.openFile(TEST_FILE, 'r')
-    f.createMatrix('m1', obj=np.ones((5, 5)))
-    f.close()
+    with omx.openFile(TEST_FILE, 'w') as f:
+        f['m2'] = np.ones((5,5))
+
+    with omx.openFile(TEST_FILE, 'r') as f:
+        f.createMatrix('m1', obj=np.ones((5, 5)))
 
 
 @with_setup(setup_func, teardown_func)
 def test_add_matrix_with_same_name():
-    f = omx.openFile(TEST_FILE, 'w')
-    add_m1_node(f)
-    # now add m1 again:
-    assert_raises(tables.NodeError, add_m1_node, f)
-    f.close()
+    with omx.openFile(TEST_FILE, 'w') as f:
+        add_m1_node(f)
+        # now add m1 again:
+        assert_raises(tables.NodeError, add_m1_node, f)
 
 
 @with_setup(setup_func, teardown_func)
 def test_get_length_of_file():
-    f = omx.openFile(TEST_FILE, 'w')
-    f['m1'] = np.ones((5,5))
-    f['m2'] = np.ones((5,5))
-    f['m3'] = np.ones((5,5))
-    f['m4'] = np.ones((5,5))
-    f['m5'] = np.ones((5,5))
-    assert(len(f)==5)
-    assert(len(f.listMatrices())==5)
-    f.close()
+    with omx.openFile(TEST_FILE, 'w') as f:
+        f['m1'] = np.ones((5,5))
+        f['m2'] = np.ones((5,5))
+        f['m3'] = np.ones((5,5))
+        f['m4'] = np.ones((5,5))
+        f['m5'] = np.ones((5,5))
+        assert(len(f)==5)
+        assert(len(f.listMatrices())==5)
+
 
 def add_m1_node(f):
     f.createMatrix('m1', obj=np.ones((7,7)))
-
