b5a8e1ac026f12450c3eea372db054df2a14c8b9
Marty <mmilkovits@camsys.com>
2016-02-05 00:33:14 -0500

    Update Tester.cs

--- api/csharp/test/TestOMX/TestOMX/Tester.cs
+++ api/csharp/test/TestOMX/TestOMX/Tester.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using HDF5DotNet;
 using CSharpOMX;
@@ -11,124 +12,166 @@ namespace CSharpOMXTester
 {
     class Tester
     {
-        static string testFile = "C:\\omx\\api\\csharp\\test\\testcsharp.omx";
-        static string testPythonFile = "C:\\omx\\api\\csharp\\test\\myfile.omx";
-        static string testRFile = "C:\\omx\\api\\r\\test.omx";
-        static string testRMtx = "PA1";
-        static string testMtx = "MtxTest";
-        static string testPythonMtx = "m1";
+        static string testCreateFile = ".\\testcsharp.omx";
+        static string testRFile = ".\\test.omx";
 
         static void Main(string[] args)
         {
-            TestFiles(testFile, testMtx);
-           // TestPythonFiles(testPythonFile, testPythonMtx);
-           // TestRFile(testRFile, testRMtx);
+            string testMtx = "tempMat";
+            CreateMatrixTest(testCreateFile);
+            ReadTest(testCreateFile);
+            //ReadTest(testRFile); //- FAILS - cannot read R file attributes
+            ReadWriteTablesTest(testCreateFile, testMtx);
+            ReadWriteRowTest(testCreateFile, testMtx);
+            MappingTest(testCreateFile, testMtx);
+            Console.ReadLine();
         }
 
-        public static void TestFiles(string testCSharp, string testMtx)
+
+        // test that we can create an empty file and add matrices
+        public static void CreateMatrixTest(string file)
         {
-            long[] shape = {200, 200};
+            int zones = 3;
+            double[,] testblock;
+            string[] matrixNames = { "mat1", "mat2" };
+            OmxWriteStream ws = OmxFile.Create(file, zones, true);
+
+            // NOTE: cannot create data type until after file stream is created
+            H5DataTypeId matrixDataTypes = H5T.copy(H5T.H5Type.NATIVE_DOUBLE);
+            for (int i = 0; i < matrixNames.Length; i++)
+                ws.AddMatrix(matrixNames[i], matrixDataTypes);
+
+            ws.Close();
+
+            OmxReadStream rs = OmxFile.OpenReadOnly(file);
+
+            for (int i = 0; i < matrixNames.Length; i++)
+                testblock = rs.GetMatrixBlock<double>(matrixNames[i], 0, 0, 1, 1);
+
+            Console.WriteLine("mat shape is {0},{1}", rs.Shape[0], rs.Shape[1]);
+            Console.WriteLine("mat names are {0},{1}", rs.MatrixNames[0], rs.MatrixNames[1]);
+            Console.WriteLine("mat data type: {0},{1}",
+                H5T.getClass(rs.GetMatrixDataType(matrixNames[0])),
+                H5T.getClass(rs.GetMatrixDataType(matrixNames[1])));
+            rs.Close();
+
+        }
+
+        // Test that we can read an OMX file with attributes and tables
+        public static void ReadTest(string file)
+        {
+            OmxReadStream rs = OmxFile.OpenReadOnly(file);
+            Console.WriteLine("OMX version is {0}", rs.OmxVersion);
+            Console.WriteLine("mat shape is {0},{1}", rs.Shape[0], rs.Shape[1]);
+            Console.WriteLine("first mat names is {0}", rs.MatrixNames[0]);
+            Console.WriteLine("first mat data type is {0}",H5T.getClass(rs.GetMatrixDataType(rs.MatrixNames[0])));
+            rs.Close();
+        }
+
+        // test that we can read/write with a created matrix tables
+        public static void ReadWriteTablesTest(string testCSharp, string testMtx)
+        {
+            int zones = 200;
 
             //make a file and some data
-            OmxFile testOmxOutFile = new OmxFile(testCSharp, shape);
-            testOmxOutFile.CreateFileOMX();
+            OmxWriteStream ws = OmxFile.Create(testCSharp, zones, true);
 
-            UInt16[,] MtxTest = new UInt16[shape[0],shape[1]];
+            double[,] MtxTest = new double[zones, zones];
 
-            for (UInt16 i = 0; i < shape[0]; i++)
+            for (int i = 0; i < zones; i++)
             {
-                for (UInt16 j = 0; j < shape[1]; j++)
+                for (int j = 0; j < zones; j++)
                 {
-                    MtxTest[i,j] = Convert.ToUInt16(i + j);
+                    MtxTest[i, j] = Convert.ToDouble(i + j);
                 }
             }
 
-            H5DataTypeId matrixTypeId = new H5DataTypeId(H5T.H5Type.NATIVE_USHORT);
-            OmxMatrix testOmxOutMtx = new OmxMatrix(testOmxOutFile, testMtx, matrixTypeId);
-            testOmxOutMtx.FillInt16Matrix(MtxTest);
-            testOmxOutFile.Close();
+            H5DataTypeId matrixTypeId = H5T.copy(H5T.H5Type.NATIVE_DOUBLE);
+            ws.AddMatrix(testMtx, matrixTypeId);
+            ws.SetMatrix<double>(testMtx, MtxTest);
+            ws.Close();
 
             //now open the file again and read the data back in
-            OmxFile testOmxFile = new OmxFile(testCSharp, shape);
-            testOmxFile.OpenReadWrite();
-       
-       
-            OmxMatrix testInOmxMtx = new OmxMatrix(testOmxFile, testMtx, matrixTypeId);
-            UInt16[,] omxTestRead = testInOmxMtx.GetInt16Matrix();
+            OmxReadStream rs = OmxFile.OpenReadOnly(testCSharp);
+            double[,] omxTestRead = rs.GetMatrix<double>(testMtx);
 
-            int testValue = omxTestRead[4, 4];
-            Console.WriteLine("Test Value is {0} ", testValue.ToString());
-            Console.WriteLine("The file is OMX version {0}", testOmxFile.OmxVersion);
+            double testValue = MtxTest[4, 4];
+            double testValue2 = omxTestRead[4, 4];
 
-            int testValue2 = omxTestRead[4, 4];
-
-            if (testValue == testValue2){
-              Console.WriteLine(" Value match success");
+            if (testValue == testValue2)
+            {
+                Console.WriteLine(" Value match success");
             }
-            else{
-              Console.WriteLine(" Value match failure");
+            else
+            {
+                Console.WriteLine(" Value match failure");
             }
+            rs.Close();
+        }
 
+        // test read/write row function
+        public static void ReadWriteRowTest(string file, string matName)
+        {
+            OmxWriteStream ws = OmxFile.OpenReadWrite(file);
+            Console.WriteLine("mat shape is {0},{1}", ws.Shape[0], ws.Shape[1]);
+
+            H5DataTypeId dt =  ws.GetMatrixDataType(matName);
+
+            // blocks we are reading need to match the data type of the matrix
+            var rowData = new double[ws.Shape[0]];
+            var rowData2 = new double[ws.Shape[0]];
+
+            rowData = ws.GetMatrixRow<double>(matName, 2);
+
+            rowData[2] = rowData[2] + 1.0;
+            ws.SetMatrixRow<double>(matName, 2, rowData);
+            rowData2 = ws.GetMatrixRow<double>(matName, 2);
+
+            if (rowData.Sum() == rowData2.Sum())
+                Console.WriteLine("Row Read/Write successful");
+            else
+                Console.WriteLine("Read/Write mismatch");
+            ws.Close();
+        }
+
+        // test that we can store and retrieve index maps
+        public static void MappingTest(string testCSharp, string testMtx)
+        {
             //test mapping the data to a new set of ids
+            OmxWriteStream ws = OmxFile.OpenReadWrite(testCSharp);
 
-            OmxFile testMapFile = new OmxFile(testCSharp, shape);
-            testMapFile.OpenReadWrite();
-
-            int[] testMap = new int[shape[0]];
-            for (int i = 0; i < shape[0]; i++)
+            int zones = (int)ws.Shape[0];
+            int[] testMap = new int[zones];
+            for (int i = 0; i < zones; i++)
             {
                 testMap[i] = i + 2;
             }
 
             string mapName = "mapping";
-            testMapFile.CreateMapping(testMap, mapName);
-            int[] testReadMap = testOmxFile.GetMapping(mapName);
+            H5DataTypeId mapData = H5T.copy(H5T.H5Type.NATIVE_INT);
+
+            // create map
+            ws.CreateMapping<int>(testMap, mapData, mapName);
+
+            // get map data type
+            H5DataTypeId returnedData = ws.GetMappingDataType(mapName);
+
+            if(mapData.GetType().Equals(returnedData.GetType()))
+                Console.WriteLine("Mapping data match");
+            else
+                Console.WriteLine("Mapping data MISMATCH");
+
+            // read map and use it to read data from matrix
+            int[] testReadMap = ws.GetMapping<int>(mapName);
             Console.WriteLine("value for map at 2 is {0}", testReadMap[2]);
 
-            int offsetsize = Convert.ToInt32(shape[0]) +2;
-            OmxMatrix testWithMap = new OmxMatrix(testMapFile, testMtx, matrixTypeId);
-            UInt16[,] omxTestReadMap =testInOmxMtx.GetInt16MatrixWithMap(testReadMap, offsetsize);
-
+            int offsetsize = Convert.ToInt32(zones) - 2;
+            double[,] omxTestReadMap = ws.GetMatrixBlock<double>(testMtx, testReadMap[0], testReadMap[0], offsetsize, offsetsize);
+                      
             Console.WriteLine("Value for matrix at 4,4 is {0}", omxTestReadMap[4,4]);
 
-            testMapFile.Close();
+            ws.Close();
         }
-
-        public static void TestPythonFiles(string testPythonFile, string testPythonMtx)
-        {
-            long[] shape = { 100, 100 };
-
-            OmxFile testOmxFile = new OmxFile(testPythonFile, shape);
-            testOmxFile.OpenReadWrite();
-
-
-            OmxMatrix testOmxMtx = new OmxMatrix(testOmxFile, testPythonMtx);
-            double[,] omxTestRead = testOmxMtx.GetDoubleMatrix();
-
-            double testValue = omxTestRead[4, 4];
-            Console.WriteLine("Test Value is {0} ", testValue.ToString());
-            Console.WriteLine("The file is OMX version {0}", testOmxFile.OmxVersion);
-            testOmxFile.Close();
-        }
-
-        public static void TestRFile(string testRFile, string testRMtx)
-        {
-            long[] shape = { 2000, 2000};
-
-            OmxFile testOmxFile = new OmxFile(testRFile, shape);
-            testOmxFile.OpenReadWrite();
-
-            // H5DataTypeId matrixTypeId = new H5DataTypeId(H5T.H5Type.NATIVE_FLOAT);
-
-            OmxMatrix testOmxMtx = new OmxMatrix(testOmxFile, testRMtx);
-            double[,] omxTestRead = testOmxMtx.GetDoubleMatrix();
-
-            double testValue = omxTestRead[4, 4];
-            Console.WriteLine("Test Value is {0} ", testValue.ToString());
-            Console.WriteLine("The file is OMX version {0}", testOmxFile.OmxVersion);
-            testOmxFile.Close();
-        }
-
     }
 }
 
